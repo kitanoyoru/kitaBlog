c62d76b5891f6061b44e1993f39ddbcccc9e881a
Alexandr Rutkovskij <kitanoyoru@protonmail.com>
2022-08-22 13:08:21 +0300

    feat: add revalidate posts feature

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,21 @@
+// Basic imports
+import PostService from "../../lib/services/PostService";
+
+// Types
+import type { NextApiRequest, NextApiResponse } from "next"
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.body.token !== process.env.REVALIDATE_TOKEN) {
+    return res.status(401).json({ message: "Invalid token." });
+  }
+
+  try {
+    const paths = await PostService.listPosts()
+      .then((posts) => posts.map(post => `/${post.id}`));
+    const promises: Promise<any>[] = [];
+    paths.forEach((path) => promises.push(res.revalidate(path)));
+    Promise.all(promises).then(() => res.json({ revalidated: true }));
+  } catch (err) {
+    return res.status(500).send("Error revalidating");
+  }
+}; 
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image"
 import IndexSEO from "../components/SEO/IndexSEO"
 import PostService from "../lib/services/PostService"
 import { HiOutlineArrowNarrowRight } from "react-icons/hi"
+import revalidatePosts from "../lib/utils/revalidatePosts";
 
 // Types
 import type { NextPage } from "next"
@@ -19,6 +20,7 @@ const Index: NextPage<IndexProps> = ({ posts }) => {
   return (
     <>
       <IndexSEO />
+      <button onClick={() => revalidatePosts(process.env.REVALIDATE_TOKEN!)}>Revalidate</button>
       <div className="bg-primary p-5 xl:p-0 min-h-screen w-full">
         <div className="max-w-7xl pt-20 pb-36 mx-auto">
           <Link href={`/${heroPost.id}`}>
